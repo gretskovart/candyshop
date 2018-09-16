6f8508710a62d0ca17dcbcaba970729ea3c7c203
Artem <gretskovart@gmail.com>
2018-09-16 20:48:22 +0400

    add luhnAlgorithm function

--- js/good.js
+++ js/good.js
@@ -359,3 +359,21 @@ var favoriteClickHandler = function () {
 };
 
 favoriteClickHandler();
+
+var luhnAlgorithm = function (cardNumber) {
+  var arr = cardNumber.toString().split(',').map(function (char, index) {
+    var digit = parseInt(char, 10);
+
+    if ((index + cardNumber.length) % 2 === 0) {
+      var digitX2 = digit * 2;
+
+      return digitX2 > 9 ? digitX2 - 9 : digitX2;
+    }
+
+    return digit;
+  });
+
+  return !(arr.reduce(function (a, b) {
+    return a + b;
+  }, 0) % 10);
+};
