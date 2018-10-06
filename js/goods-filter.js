e6656ee56feb8356304d7117cf6215254b333aae
Artem <gretskovart@gmail.com>
2018-10-07 01:21:45 +0400

    add filterByFavorite

--- js/goods-filter.js
+++ js/goods-filter.js
@@ -51,6 +51,33 @@
     window.addProductsToPage(byNutrition);
   };
 
+  var filterByFavorite = function () {
+    var favoriteCards = document.querySelectorAll('.card__btn-favorite--selected');
+    var favoriteLinks = document.querySelectorAll('.card__btn-favorite');
+
+    if (favoriteCards.length === 0) {
+      removeCards();
+      showEmptyFilter();
+    } else {
+      for (var i = 0; i < favoriteLinks.length; i++) {
+        if (!favoriteLinks[i].classList.contains('card__btn-favorite--selected')) {
+          favoriteLinks[i].closest('.catalog__card').remove();
+        }
+      }
+    }
+  };
+
+  var showEmptyFilter = function () {
+    var cards = document.querySelector('.catalog__cards');
+    var emptyFilterTemplate = document.querySelector('#empty-filters').content
+    .querySelector('.catalog__empty-filter');
+    var emptyFilter = emptyFilterTemplate.cloneNode(true);
+    var fragment = document.createDocumentFragment();
+
+    fragment.appendChild(emptyFilter);
+    cards.appendChild(fragment);
+  };
+
   // обработчик нажатий на фильтр
   var filterHandler = function (evt) {
     var target = evt.target.innerText;
@@ -71,6 +98,9 @@
         break;
       case 'Безглютеновое':
         filterByNutrition('gluten', window.productsArray);
+        break;
+      case 'Только избранное':
+        filterByFavorite();
         break;
     }
   };
